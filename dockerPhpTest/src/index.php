76a6c3761305697df5912a147c9c9b1da08c0ab5
Jonny Carlisle <jrsc-94@hotmail.co.uk>
2024-12-22 06:13:50 +0000

    main

--- dockerPhpTest/src/index.php
+++ dockerPhpTest/src/index.php
@@ -1,35 +1,10 @@
-<?php
-// Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect the value of the input field
-    $name = htmlspecialchars($_POST['name']);
-    if (empty($name)) {
-        $greeting = "Hello, Guest!";
-    } else {
-        $greeting = "Hello, " . $name . "!";
-    }
-} else {
-    $greeting = "Please enter your name!";
-}
-?>
-
-<!DOCTYPE html>
+<!DOCTYPE html/>
 <html>
 <head>
-    <title>Army Finder Home</title>
+    <title>My Portfolio</title>
 
-    <meta charset="utf-8" />
-    <meta name="description" content="40k army chooser page" />
-    <meta name="keywords" content="Warhammer 40,000, warhammer 40k, which army to choose" />
-    <meta name="author" content="Jinty" />
-    <meta name="viewport" content="width-device-width, initial-scale=1.0" />
-
-    <!-- Bootstrap CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Main style sheet -->
+    <!-- Links in the syle sheet -->
     <link rel="stylesheet" href="css/style.css" />
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 
@@ -39,215 +14,217 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Navigation -->
     <nav>
         <ul class="listSpec">
-            <li><a href="homePage.html">Home</a></li>
-            <li><a href="factions.html">Factions</a></li>
-            <li><a href="rules.html">Rules</a></li>
-            <li><a href="gallery.html">Gallery</a></li>
-            <li><a href="contactUs.html">Contact us</a></li>
+            <li><a href="#" onclick="scrollToSection('skillsSection')">Skills</a></li>
+            <li><a href="#" onclick="scrollToSection('projectsSection')">Projects</a></li>
+            <li><a href="#" onclick="scrollToSection('aboutMeSection')">About Me</a></li>
         </ul>
     </nav>
-
-    <!-- Header -->
-    <div class="container">
-        <div class="header-content">
-            <h1>Welcome to Army Finder</h1>
-            <h2>Making your army finding easier for you</h2>
-            <div class="button-container">
-                <button class="btn btn-dark">Begin</button>
-            </div>
+    <br>
+    <!-- Introduction -->
+    <div class="mainImgContainer">
+        <div class="introHeader">
+        <header>
+            <h1>Hi, I am Jonny || Junior Software Developer</h1>
+        </header>
+        <p>
+            I greatly enjoy the challenge of creating and developing software, as well as learning new skills.
+            I am also a gamer, model maker and a bit of a sci-fi nerd. 
+        <p>
         </div>
     </div>
 
-    <!-- <h2>Welcome to Army Finder</h2> -->
-    <hr />
     <br><br>
+    
+    <!-- Skills Section -->
+    <section id="skillsSection">
+        <h2>Skills</h2>
 
-    <!-- Background picture -->
-    <!--<img class="bckImg" alt="dark sandy background where 2 titans and multiple space marines fighting " src="images/backgrounds/battleBegins.jpg" />
-    -->
-    <br><br>
-    <!-- Main Content -->
-    <p>
-        Warhammer 40,000 has many army choices for players.
-        Sometimes that makes it hard to decide what to choose or how to get into the hobby.
-        Here you can select an army and get a brief introduction to what that army is about
-        and will show some of the best places that you can buy the models from. No more fuss,
-        but instead unlimited power at your fingertips. Click an army icon to begin your journey
-    </p>
+        <!-- Program Languages -->
+        <div class="iconContainer">
+            <h3>Program Languages</h3>
+            <figure class="icon-figure">
+                <i class="fa-brands fa-html5"></i>
+                <figcaption>HTML 5</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-regular fa-file-code"></i>
+            <figcaption>CSS</figcaption>
+            </figure>        
+            <figure class="icon-figure">
+                <i class="fa-brands fa-php"></i>
+                <figcaption>PHP</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-brands fa-js"></i>
+                <figcaption>JS</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-solid fa-scroll"></i>
+            <figcaption>jQUERY</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-solid mysqlIco fa-database"></i>
+                <figcaption>mySQL</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-solid sqlIco fa-database"></i>
+                <figcaption>SQL</figcaption>
+            </figure>
+        </div>
+        
+        <br>
 
+        <!-- Program Tools -->
+        <div class="iconContainer">
+            <h3>Tools</h3>
+            <figure class="icon-figure">
+                <i class="fa-brands fa-git-alt"></i>
+                <figcaption>Git</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-brands fa-github"></i>
+                <figcaption>Github</figcaption>
+            </figure>
+            
+            <figure class="icon-figure">
+                <i class="fa-brands fa-aws"></i>
+                <figcaption>AWS</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-solid fa-code-commit"></i>
+                <figcaption>AWS S3</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-brands fa-docker"></i>
+                <figcaption>Docker</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-solid fa-bug"></i>
+                <figcaption>Zendesk</figcaption>
+            </figure>
+            
+            <br>
+            
+            <figure class="icon-figure">
+            <i class="fa-solid fa-laptop-code"></i>
+                <figcaption>Visual Studio</figcaption>
+            </figure>
+            <figure class="icon-figure">
+                <i class="fa-solid fa-server"></i>
+                <figcaption>SQL Workbench</figcaption>
+            </figure>
+        </div>
+    </section>
+    
     <br>
+    <br><br>
+    <!-- Projects Section -->
+    <section id="projectsSection">
+        <h2>Projects</h2>
+        <div class="projectContainer">
+         
+            <h3>Auto-generated reports</h3>
+            <p>
+                This project was created to help the clients of the company I worked for, Wearelanded, to generate reports automatically.
+                The reports are generated from the data stored in the database. What the clients choose to see in the report is what they got. 
+            </p>
+            <row class="projectSkills">
+                <div class="rSkill"><span>php</span></div>
+                <div class="rSkillB"><span>mySQL</span></div>
+                <div class="rSkill"><span>SQL</span></div>
+                <div class="rSkill"><span>js</span></div>
+                <div class="rSkillB"><span>jQuery</span></div>
+            </row>
+        </div>
+        <br>
 
-    <h4>Imperium</h4>
+        <div class="projectContainer">
+        <br>
+            <h3>Upgrades bootstap styles and functionality</h3>
+            <p>
+                This project was during my time at Wearelanded and it was to update the platform from bootstrap 4 to bootstrap 5. 
+                While this was going on functionality was also updated and improved.
+            </p>
+            <row class="projectSkills">
+                <div class="rSkill"><span>php</span></div>
+                <div class="rSkill"><span>css</span></div>
+                <div class="rSkill"><span>js</span></div>
+                <div class="rSkillB"><span>jQuery</span></div>
+            </row>
+        </div>
+        <br>
 
-    <!-- Space marine icons -->
-    <div class="sMIcons">
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_DarkAngels.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_WhiteScars.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_SpaceWolves.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/Imperial_Fists_Icon.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_BloodAngels.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icon_Iron_Hands.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_UltraMarines.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_Salamanders.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_RavenGuard.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/spaceMarineIcons/GH_Icons_BlackTemplars.png" />
-        </a>
-    </div>
+        <div class="projectContainer">
+        <br>
+            <h3>Functionality and style upgrades</h3>
+            <p>
+                This project was during my time at Zymplify and it was to update the css and overall functionality of the platform. 
+                This involved updating modals, buttons, and the overall look of the platform,
+                as well as moving inline styles and internal styles to external stylesheets and reducing the amout of external stylesheets
+            </p>
+            <row class="projectSkills">
+                <div class="rSkill"><span>php</span></div>
+                <div class="rSkill"><span>css</span></div>
+                <div class="rSkill"><span>js</span></div>
+                <div class="rSkillB"><span>jQuery</span></div>
+            </row>
+        </div>
 
-    <!-- Imperium -->
-    <div class="impIcons">
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/AstraMilitarum_Icon.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/GH_Icons_Deathwatch.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/Custodes_Icon.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/GH_Icons_Adeptus_Mechanicus.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/GH_Icons_Grey_Knights.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/GH_Icons_Sisters.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/imperium/Knights.png" />
-        </a>
-    </div>
+        <br>
+        
+        <div class="projectContainer">
+            <h3>Round-robin integration</h3>
+            <p>
+                This project was also during my time at Zymplify and was created to assign multiple candidates to multiple employees. In order to process the workload more efficiently.
+                This was applied to both form submissions and could also be manually set by the clients themselves in other areas of the system.
+            </p>
+            <row class="projectSkills">
+                <div class="rSkill"><span>php</span></div>
+                <div class="rSkillB"><span>mySQL</span></div>
+                <div class="rSkill"><span>SQL</span></div>
+                <div class="rSkill"><span>js</span></div>
+                <div class="rSkillB"><span>jQuery</span></div>
+            </row>
+        </div>
+     </section>
+     <br><br><br>
 
+     <!-- Projects Section -->
+    <section id="aboutMeSection">
+        <h2>About Me</h2>
+        <div class="aboutMeContainer">
+            <p>
+                My other hobbies include gaming, model making, and sci-fi. I have always been a fan of sci-fi, used to be just movies and tv shows but now I have started reading sci-fi books as well.
+                This includes the horus heresy series, a series I would love to see on the big screen one day. In terms of gaming, I am a fan of the total war series, the elder scrolls series, and the fallout series.
+                Ever since I was a child I have always been interested in technology and how things work. 
+            </p>
+            <p>
+                From taking apart my toys to see how they functioned to eventually 3d printing an iron man helmet, which I later kitted out to light up and open and close at the push of a button.
+                I have always been interested in how things work and how they can be improved. 
+                I believe this carries over to web and software development too, where I hope to expand my knowledge and skills.
+                If you wish to contact me you can do so by email at: <a href="mailto:jrsc-94@hotmail.co.uk" class="email-link">jrsc-94@hotmail.co.uk</a>
+            </p>
+
+            <a class="cvL" href="assets/cv.pdf" download="My_CV.pdf"><strong>Download My CV</strong></a>
+        </div>
+    </section>
     <br><br>
 
-    <!-- Xenos -->
-    <h4>Xenos / Unaffiliated</h4>
-
-    <div class="impIcons">
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/GH_Icon_Votann.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/GH_Icons_Necron.png" />
-        </a>
-        <a href="orks.html">
-            <img class="aFIcons" src="images/xenos/GH_Icons_Orks.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/Tau_Icon.png" />
-        </a>
-
-        <!-- Aeldari -->
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/aeldari/aeldari_Icon.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/aeldari/GH_Icons_Drukhari.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/aeldari/Harleyquins.png" />
-        </a>
-
-        <!-- Hivemind -->
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/hivemind/GH_Icons_Tyranids.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/xenos/hivemind/GSC_Icon.png" />
-        </a>
-    </div>
-
-    <br><br>
-
-    <h4>Chaos</h4>
-    <!-- Chaos Space Marines -->
-    <div class="chaoIcons">
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_EmpChildren.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_IronWarriors.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_NightLords.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_WorldEaters.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_DeathGuard.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_ThousandSon.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_BlackLegion.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_WordBearers.png" />
-        </a>
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/chaosSpaceMarinesIcons/GH_Icons_AlphaLegion.png" />
-        </a>
-    </div>
-
-    <!-- Chaos -->
-    <div class="chaoIcons">
-        <!--<a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/GH_Icons_Daemons.png" />
-        </a>-->
-        <a href="dAPage.html">
-            <img class="aFIcons" src="images/chaos/The_Chaos_Knights.png" />
-        </a>
-    </div>
-    <br><br>
-    <!--Footer-->
+    <!-- Footer -->
     <footer>
         <div class="footer-content">
-            <p>&copy; 2024 Army Picker. All rights reserved</p>
-
-            <!--Footer Navigation-->
-            <ul class="footerNav">
-                <li><a href="homePage.html">Home</a></li>
-                <li><a href="dAPage.html">Factions</a></li>
-                <li><a href="rules.html">Rules</a></li>
-                <li><a href="gallery.html">Gallery</a></li>
-                <li><a href="contactUs.html">Contact us</a></li>
-            </ul>
-
-            <p class="footerSocial">
-                <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                <a href="#"><i class="fa-brands fa-discord"></i></a>
-            </p>
+            <p>&copy; 2024 Jonny Calisle's Portfolio. All rights reserved</p>
         </div>
     </footer>
 
-    <!-- Bootstrap JavaScript and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    <!-- Scripts -->
+    <script>
+        function scrollToSection(sectionId) {
+            const element = document.getElementById(sectionId);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    </script>
 </body>
 </html>
