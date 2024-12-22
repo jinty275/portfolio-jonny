2128d6d217ae7d3323a753e079ce1742a24468df
Jonny Carlisle <jrsc-94@hotmail.co.uk>
2024-12-22 15:24:23 +0000

    main

---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+function scrollToSection(event, sectionId) {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
